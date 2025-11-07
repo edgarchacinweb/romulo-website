a699f26bdfd14404c053f268ac861250838aab8c
edgarchacinweb <edgar.chacin2005@outlook.com>
2025-11-06 20:56:07 -0400

    feat: resuelto bug critico de seguridad al entrar al panel de administrador sin estar logueado como administrador

--- src/scripts/dashboard.js
+++ src/scripts/dashboard.js
@@ -1,6 +1,23 @@
-const role = localStorage.getItem("role");
-if (!role || role !== "administrador")
-  window.location.href = "/app/iniciar-sesion.html";
+const token = localStorage.getItem("auth");
+if (!token) window.location.href = "/app/iniciar-sesion.html";
+
+(async () => {
+  const response = await fetch(`${window.APP_CONFIG.api_url}/user/get`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = await response.json();
+
+  if (data.Rol !== "administrador") {
+    localStorage.removeItem("auth");
+    localStorage.removeItem("role");
+    window.location.href = "/app/iniciar-sesion.html";
+  }
+})();
 
 document.addEventListener("DOMContentLoaded", () => {
   // Botón para cerrar sesión
@@ -9,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const logout = confirm("¿Estás seguro de que quieres cerrar sesión?");
     if (logout) {
-      localStorage.removeItem("token");
+      localStorage.removeItem("auth");
       localStorage.removeItem("role");
       window.location.href = "/";
     }
