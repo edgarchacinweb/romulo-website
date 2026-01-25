38f0606aadb26108b1bc51834fbd7bff344083b4
edgarchacinweb <edgar.chacin2005@outlook.com>
2026-01-25 15:10:25 -0400

    feat: Agregada carga de datos en la pagina de edicion de los datos del representante

--- src/scripts/editparentprofile.js
+++ src/scripts/editparentprofile.js
@@ -2,11 +2,96 @@ import authorize from "./auth.js";
 
 authorize("representante");
 
-document.addEventListener("DOMContentLoaded", () => {
+const loader = document.createElement("loader-spinner");
+document.body.appendChild(loader);
+
+const token = localStorage.getItem("auth") ?? "";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // Campos del formulario
+  const photoPreview = document.getElementById("photoPreview");
+  const photoField = document.getElementById("photoUploadInput");
+  const firstNameField = document.getElementById("firstNameField");
+  const lastNameField = document.getElementById("lastNameField");
+  const identityField = document.getElementById("identityField");
+  const genderField = document.getElementById("genderField");
+  const emailField = document.getElementById("emailField");
+  const phoneField = document.getElementById("phoneField");
+  const occupationField = document.getElementById("occupationField");
+  const addressField = document.getElementById("addressField");
+
+  // Icono de la previsualización de la foto de perfil
+  const uploadIcon = photoPreview.querySelector(".upload-icon");
+
+  // Cargando datos del representante
+  try {
+    // Obteniendo datos
+    const parentDataResponse = await fetch(
+      `${window.APP_CONFIG.api_url}/people/get`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    if (!parentDataResponse.ok) throw Error();
+    const parentData = await parentDataResponse.json();
+
+    // Cargando datos del usuario
+    const parentUserDataResponse = await fetch(
+      `${window.APP_CONFIG.api_url}/user/get`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    if (!parentUserDataResponse.ok) throw Error();
+    const parentUserData = await parentUserDataResponse.json();
+
+    // Cargando foto de perfil
+    const profilePhotoResponse = await fetch(
+      `${window.APP_CONFIG.api_url}/docs/get/carnet-${parentData.DatosPersonaId}.webp`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "image/webp",
+        },
+      },
+    );
+
+    if (profilePhotoResponse.ok) {
+      const profilePhoto = await profilePhotoResponse.blob();
+      const parentProfilePhoto = URL.createObjectURL(profilePhoto);
+      photoPreview.style.backgroundImage = `url(${parentProfilePhoto})`;
+      uploadIcon.style.display = "none";
+    }
+
+    // Cargando datos en los campos del formulario
+    firstNameField.value = parentData["Nombre"] ?? "";
+    lastNameField.value = parentData["Apellido"] ?? "";
+    identityField.value = `V-${parentData["Cedula"]}` ?? "";
+    genderField.value = parentData["Sexo"] ?? "";
+    emailField.value = parentUserData["Email"] ?? "";
+    phoneField.value = parentData["Telefono"] ?? "";
+    occupationField.value = parentData["Ocupacion"] ?? "";
+    addressField.value = parentData["Direccion"] ?? "";
+
+    loader.remove();
+  } catch (error) {
+    alert("Ocurrió un error al cargar los datos del representante...");
+    window.location.href = "/app/representante/inicio/";
+    return;
+  }
+
   // --- Funcionalidad: Previsualización de Foto de Perfil ---
   const photoInput = document.getElementById("photoUploadInput");
-  const photoPreview = document.getElementById("photoPreview");
-  const uploadIcon = photoPreview.querySelector(".upload-icon");
 
   photoInput.addEventListener("change", function (e) {
     const file = e.target.files[0];
