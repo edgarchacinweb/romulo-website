17e0ad8d924908b27898ab7c88a02722f35bf058
edgarchacinweb <edgar.chacin2005@outlook.com>
2026-03-09 16:31:52 -0400

    feat: Agregada actualización de período de carga de calificaciones

--- src/components/notifications-container.component.js
+++ src/components/notifications-container.component.js
@@ -11,7 +11,7 @@ class NotificationContainerComponent extends HTMLElement {
           position: fixed;
           top: 0;
           left: 0;
-          z-index: 1000;
+          z-index: 10000;
           width: 100vw;
           min-height: 100vw;
           display: flex;
