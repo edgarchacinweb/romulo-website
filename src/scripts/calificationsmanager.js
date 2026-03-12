8fabf0127687070678b1670919afe8cfaa8e7ff3
edgarchacinweb <edgar.chacin2005@outlook.com>
2026-03-12 03:59:15 -0400

    feat: guardado y actualización de calicaciones desde el docente

--- src/scripts/calificationsmanager.js
+++ src/scripts/calificationsmanager.js
@@ -6,6 +6,7 @@ const token = localStorage.getItem("auth");
 
 /* DOM Elements */
 const notifications = document.getElementById('notifications');
+const gradesLapsos = document.getElementById('gradesLapsos');
 const selectEstudiante = document.getElementById('selectEstudiante');
 const selectPeriodo = document.getElementById('selectPeriodo');
 const selectGrado = document.getElementById('selectGrado');
@@ -27,6 +28,8 @@ let terms = [];
 let grades = [];
 let students = [];
 let subjects = [];
+let lapses = {};
+let studentCalifications = [];
 
 const emptyStateDissapear = () => {
     emptyState.classList.remove('hidden');
@@ -159,6 +162,24 @@ document.addEventListener("DOMContentLoaded", async () => {
             }
         });
 
+        // Carga de lapsos
+        const lapsesPromise = await fetch(`${window.APP_CONFIG.api_url}/lapsos/current`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        const lapsesResponse = await lapsesPromise.json();
+        if (!lapsesPromise.ok) {
+            throw new Error("Error al cargar los lapsos");
+        } else if (lapsesResponse.length === 0) {
+            throw new Error("No hay lapsos registrados");
+        }
+
+        lapses = { ...lapsesResponse };
+
         const subjectsResponse = await subjectsPromise.json();
         if (!subjectsPromise.ok) {
             throw new Error("Error al cargar las materias");
@@ -167,7 +188,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
 
         subjects = [...subjectsResponse];
-        console.log(subjects);
+
         // Carga de periodos escolares
         const termsResponse = await termsPromise.json();
         if (!termsPromise.ok) {
@@ -216,33 +237,71 @@ selectSeccion.addEventListener('change', async () => {
     await loadStudents();
 });
 
-/* Mock Data */
-const materias = [
-    "Matemática",
-    "Lengua y Literatura",
-    "Historia de Venezuela",
-    "Biología",
-    "Física",
-    "Química",
-    "Educación Física",
-    "Artes Plásticas"
-];
-
 /* Render Table */
 function renderGradesTable() {
     gradesBody.innerHTML = '';
+    gradesLapsos.innerHTML = '';
+    let flags = [];
 
-    materias.forEach((materia, index) => {
+    lapses.lapsos.forEach((lapso, index) => {
+        const calificationsTerm = new Date(lapso.fecha_fin);
+        calificationsTerm.setDate(calificationsTerm.getDate() - 7);
+        const currentDate = new Date();
+        const flag = calificationsTerm <= currentDate ? "active" : "locked";
+        flags.push(flag);
+        const lapsoDiv = document.createElement('div');
+        lapsoDiv.classList.add('grades__lapso');
+        lapsoDiv.classList.add(`grades__lapso--${flag}`);
+        lapsoDiv.innerHTML = `
+            ${flag === "locked" ? `
+            <svg class="grades__lapso-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M18 8H17V6C17 3.24 14.76 1 12 1C9.24 1 7 3.24 7 6V8H6C4.9 8 4 8.9 4 10V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V10C20 8.9 19.1 8 18 8ZM9 6C9 4.34 10.34 3 12 3C13.66 3 15 4.34 15 6V8H9V6ZM18 20H6V10H18V20ZM12 17C13.1 17 14 16.1 14 15C14 13.9 13.1 13 12 13C10.9 13 10 13.9 10 15C10 16.1 10.9 17 12 17Z"
+                    fill="#A0AABF" />
+            </svg>
+            ` : `<span class="grades__lapso-dot"></span>`}
+            <div class="grades__lapso-info">
+                <span class="grades__lapso-name">Lapso ${lapso.lapso}</span>
+                <span class="grades__lapso-status">${flag === "active" ? "✓ Editable" : "🔒 Bloqueado"}</span>
+            </div>
+        `;
+        gradesLapsos.appendChild(lapsoDiv);
+    });
+
+    const student = students.find(student => student.EstudianteId === selectEstudiante.value);
+    const level = student.Curso.Grado < 4 ? "Secundaria" : "Bachillerato";
+    const selectedSubjects = subjects.filter(subject => subject.Nivel === level);
+
+    selectedSubjects.forEach((materia, index) => {
         const tr = document.createElement('tr');
 
         tr.innerHTML = `
-            <td>${materia}</td>
-            <td><input type="number" class="grades__input js-grade" data-materia="${index}" min="0" max="20" placeholder="-"></td>
-            <td><input type="number" class="grades__input js-grade" data-materia="${index}" min="0" max="20" placeholder="-"></td>
-            <td><input type="text" class="grades__input grades__input--locked" value="-" readonly></td>
-            <td class="grades__promedio js-promedio" data-materia="${index}">-</td>
-        `;
-
+            <td>${materia.Nombre}</td>
+        `
+        flags.forEach((flag, index) => {
+            const td = document.createElement('td');
+            const lapse_id = lapses.lapsos.find(l => l.lapso === index + 1).lapso_id;
+            const calification = studentCalifications.find(c => c.LapsoId === lapse_id && c.MateriaId === materia.MateriaId)?.Ponderacion ?? "";
+            if (flag === "active") {
+                td.innerHTML = `
+                <input
+                    type="number"
+                    class="grades__input js-grade"
+                    data-lapso="${lapse_id}"
+                    data-id="${materia.MateriaId}"
+                    data-materia="${index}"
+                    min="0"
+                    max="20"
+                    value="${calification}"
+                    placeholder="-"
+                >
+                `;
+            } else {
+                td.innerHTML = `<input type="text" class="grades__input grades__input--locked" value="-" readonly>`;
+            }
+            tr.appendChild(td);
+        });
+        tr.innerHTML += `<td class="grades__promedio js-promedio" data-materia="${index}">-</td>`;
         gradesBody.appendChild(tr);
     });
 
@@ -250,6 +309,8 @@ function renderGradesTable() {
     inputs.forEach(input => {
         input.addEventListener('input', handleGradeInput);
     });
+
+    calculateAverages();
 }
 
 /* Validations and Calculations */
@@ -271,6 +332,14 @@ function calculateAverages() {
 
     promedios.forEach((promedioCell, index) => {
         const rowInputs = document.querySelectorAll(`.js-grade[data-materia="${index}"]`);
+        if (rowInputs.length < 3) return;
+        const calculate = Array.from(rowInputs).every(input => input.value !== '');
+        if (!calculate) {
+            promedioCell.textContent = '-';
+            promedioCell.classList.remove('grades__promedio--blue', 'grades__promedio--red');
+            return;
+        }
+
         let sum = 0;
         let count = 0;
 
@@ -280,6 +349,7 @@ function calculateAverages() {
                 count++;
             }
         });
+
 
         if (count > 0) {
             const avg = (sum / count).toFixed(2);
@@ -318,7 +388,7 @@ function checkFormCompletion() {
 }
 
 /* View Toggles */
-selectEstudiante.addEventListener('change', (e) => {
+selectEstudiante.addEventListener('change', async (e) => {
     if (e.target.value) {
         emptyState.classList.add('hidden');
         gradesSection.classList.remove('hidden');
@@ -329,24 +399,31 @@ selectEstudiante.addEventListener('change', (e) => {
         studentNameAlert.textContent = name;
         studentNameTitle.textContent = name;
 
+        // Cargando calificaciones del estudiante
+        const studentCalificationsPromise = await fetch(`${window.APP_CONFIG.api_url}/calification/student/${student.EstudianteId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        })
+
+        const studentCalificationsResponse = await studentCalificationsPromise.json();
+        if (!studentCalificationsPromise.ok) {
+            console.error(studentCalificationsResponse);
+            const notification = document.createElement('notification-component');
+            notification.setAttribute('type', 'error');
+            notification.setAttribute('message', "Error al cargar las calificaciones del estudiante");
+            notifications.appendChild(notification);
+            studentCalifications = [];
+            return;
+        };
+        studentCalifications = [...studentCalificationsResponse];
+
         renderGradesTable();
         checkFormCompletion();
     }
 });
-
-// btnClear.addEventListener('click', () => {
-//     selectEstudiante.value = "";
-//     document.getElementById('selectGrado').value = "";
-//     document.getElementById('selectSeccion').value = "";
-//     document.querySelector('.filters__input').value = "";
-
-//     emptyState.classList.remove('hidden');
-//     gradesSection.classList.add('hidden');
-//     studentAlert.classList.add('hidden');
-
-//     btnSave.disabled = true;
-//     btnExport.disabled = true;
-// });
 
 /* Outro Animation */
 btnLogout.addEventListener('click', () => {
@@ -355,4 +432,54 @@ btnLogout.addEventListener('click', () => {
     setTimeout(() => {
         window.location.href = '/app/admin/dashboard/';
     }, 500);
+});
+
+btnSave.addEventListener('click', async () => {
+    const loader = document.createElement('loader-component');
+    document.body.appendChild(loader);
+    document.body.style.pointerEvents = "none";
+    try {
+        const gradesInputs = document.querySelectorAll('.js-grade');
+        const grades = [];
+        gradesInputs.forEach(input => {
+            grades.push({
+                MateriaId: input.getAttribute('data-id'),
+                Ponderacion: input.value,
+                EstudianteId: selectEstudiante.value,
+                LapsoId: input.getAttribute('data-lapso')
+            });
+        });
+
+        const saveGradesPromise = await fetch(`${window.APP_CONFIG.api_url}/calification/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(grades)
+        });
+
+        if (!saveGradesPromise.ok) {
+            const saveGradesResponse = await saveGradesPromise.json();
+            throw new Error(saveGradesResponse.message);
+        }
+
+        const notification = document.createElement('notification-component');
+        notification.setAttribute('type', 'success');
+        notification.setAttribute('text', "Calificaciones guardadas correctamente");
+        notifications.appendChild(notification);
+        studentCalifications = [...grades];
+        btnSave.disabled = true;
+        btnExport.disabled = false;
+    } catch (error) {
+        console.error(error.stack);
+        const notification = document.createElement('notification-component');
+        notification.setAttribute('type', 'error');
+        notification.setAttribute('message', error.message);
+        notifications.appendChild(notification);
+        notification.focus();
+    } finally {
+        loader.remove();
+        document.body.style.pointerEvents = "auto";
+    }
 });
